93a72545632696186fa8aa00782fae3359bab4bc
Diego Graciano <diegograciano@Diegos-MBP.home>
2022-09-05 15:06:58 -0500

    Formated dates and numbers, added table header color

---
+++ src/consts/consts.js
@@ -0,0 +1,112 @@
+const cryptos = [
+    {
+        name: 'bitcoin',
+        shortName: 'BTC',
+        pairID: '945629',
+        backPos: '-392px'
+    },
+    {
+        name: 'Stellar',
+        shortName: 'XLM',
+        pairID: '1061451',
+        backPos: '-98px'
+    },
+    {
+        name: 'Basic Attention Token',
+        shortName: 'BAT',
+        pairID: '1061774',
+        backPos: '-392px'
+    },
+    {
+        name: 'Etherium',
+        shortName: 'ETH',
+        pairID: '1061443',
+        backPos: '-308px'
+    },
+    {
+        name: 'Cardano',
+        shortName: 'ADA',
+        pairID: '1062537',
+        backPos: '-322px'
+    },
+    {
+        name: 'EOS',
+        shortName: 'EOS',
+        pairID: '1061444',
+        backPos: '-224px'
+    },
+    {
+        name: '0x',
+        shortName: 'ZRX',
+        pairID: '1061803',
+        backPos: '-392px'
+    },
+    {
+        name: 'IOTA',
+        shortName: 'MIOTA',
+        pairID: '1061449',
+        backPos: '-392px'
+    },
+    {
+        name: 'Ontology',
+        shortName: 'ONT',
+        pairID: '1075377',
+        backPos: '-392px'
+    },
+    {
+        name: 'Ravecoin',
+        shortName: 'VEN',
+        pairID: '1094142',
+        backPos: '-392px'
+    },
+    {
+        name: 'Tron',
+        shortName: 'TRX',
+        pairID: '1061450',
+        backPos: '-392px'
+    },
+    {
+        name: 'Litecoin',
+        shortName: 'LTC',
+        pairID: '1061445',
+        backPos: '-392px'
+    },
+    {
+        name: 'NEO',
+        shortName: 'NEO',
+        pairID: '1061409',
+        backPos: '-392px'
+    },
+    {
+        name: 'Pundi X[old]',
+        shortName: 'NPXS',
+        pairID: '1094135',
+        backPos: '-392px'
+    },
+    {
+        name: 'Binance',
+        shortName: 'BNB',
+        pairID: '1061448',
+        backPos: '-392px'
+    },
+    {
+        name: 'Bitcoin cash',
+        shortName: 'BCH',
+        pairID: '1061410',
+        backPos: '-392px'
+    },
+    {
+        name: 'XRP',
+        shortName: 'XRP',
+        pairID: '1057392',
+        backPos: '-392px'
+    },
+    {
+        name: 'Chainlink',
+        shortName: 'LINK',
+        pairID: '1061794',
+        backPos: '-392px'
+    },
+];
+
+export default { cryptos }
---
+++ src/tables.jsx
@@ -0,0 +1,284 @@
+import React from "react";
+import cryptos from './consts/consts';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import SwipeableViews from 'react-swipeable-views';
+import {
+    Grid,
+    Typography,
+    Tabs,
+    Tab,
+    Box,
+    Button,
+    Dialog,
+    Select,
+    MenuItem
+} from '@mui/material';
+import { useEffect } from "react";
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const TabPanel = (props) => {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
+export default function Tables() {
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+    const [open, setOpen] = React.useState(false);
+    const [cryptosData, setCryptosData] = React.useState([]);
+
+    useEffect(() => {
+        //updateCryptos();
+    }, []);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index) => {
+        setValue(index);
+    };
+
+    const updateCryptos = async () => {
+        // Update crypto currencies
+        setOpen(true);
+        let tempData = cryptos.cryptos;
+        for (let i = 0; i < tempData.length; i++) {
+            const crypto = tempData[i];
+            const cryptoData = await callInvesting(crypto.pairID, 'P1W', 'P1D', 120);
+            const newValue = cryptoData.data.data;
+            // Format date
+            newValue.forEach(value => {
+                value[0] = getDate(value[0]);
+            });
+            tempData[i]['data'] = newValue;
+        }
+        setCryptosData(tempData);
+        setOpen(false);
+    }
+
+    const callInvesting = (pairId, period, interval, pointscount) => {
+        return axios({
+            method: 'GET',
+            url: `https://api.investing.com/api/financialdata/${pairId}/historical/chart/`,
+            params: {
+                period,
+                interval,
+                pointscount,
+            },
+        });
+    }
+
+    const getDate = (unixEpoch) => {
+        var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+        d.setUTCMilliseconds(unixEpoch);
+        d = d.toISOString().split('T')[0];
+        const [year, month, day] = d.split('-');
+        const result = [month, day, year].join('/');
+        return result;
+    }
+
+    const copyRows = (row1, row2) => {
+        let copied = '<table><tr>';
+        row1.forEach(item => {
+            (item != 0) && (copied = copied + `<td>${item}</td>`);
+        });
+        copied = copied + '</tr><tr>';
+        row2.forEach(item => {
+            (item != 0) && (copied = copied + `<td>${item}</td>`);
+        });
+        copied = copied + '</tr></table>';
+        const blob = new Blob([copied], { type: "text/html" });
+        const tableHtml = new window.ClipboardItem({ "text/html": blob });
+
+        navigator.clipboard.write([tableHtml]).then(function () {
+        }, function (err) {
+            console.error('Async: Could not copy text: ', err);
+        });
+    }
+
+
+
+    return (
+        <div>
+            <Grid style={{ margin: '2vw' }}>
+                <Grid container>
+                    <Dialog open={open}>
+                        <Grid style={{ margin: '40px' }}>
+                            <h1>Making magic...</h1>
+                            <CircularProgress />
+                        </Grid>
+                    </Dialog>
+                    <Grid item textAlign={'left'} xs={6}>
+                        <h1>Hola Juan Carlos 👋🏻</h1>
+                        <h3>Cryptosmart Web v1.0.1</h3>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Button variant="outlined" style={{ marginTop: '50px' }} onClick={() => updateCryptos()}>Actualizar datos</Button>
+                    </Grid>
+                </Grid>
+                <Grid>
+                    <Box sx={{ maxWidth: { xs: '100%', sm: '100%' }, bgcolor: 'background.paper' }}>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            variant="scrollable"
+                            scrollButtons="auto"
+                            allowScrollButtonsMobile
+                            aria-label="scrollable auto tabs example"
+                        >
+                            <Tab label='Resumen' icon={'📊 '}></Tab>
+                            {cryptos.cryptos.map((crypto, i) =>
+                                <Tab label={crypto.shortName} icon={
+                                    <div style={{
+                                        width: '14px',
+                                        height: '11px',
+                                        overflow: 'hidden',
+                                        backgroundImage: "url('https://i-invdn-com.investing.com/next_/images/components/flags/currency-v3.svg')",
+                                        backgroundPositionX: crypto.backPos
+                                    }} />
+                                }></Tab>
+                            )}
+                        </Tabs>
+                        <SwipeableViews
+                            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                            index={value}
+                            onChangeIndex={handleChangeIndex}
+                        >
+                            <TabPanel>
+                                <Grid textAlign={'left'}>
+                                    <h3>Resumen</h3>
+                                </Grid>
+                                <TableContainer component={Paper} style={{maxHeight: '60vh', height: '60vh'}}>
+                                    <Table sx={{ minWidth: 650 }} aria-label="simple table" stickyHeader>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Date</TableCell>
+                                                <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Value</TableCell>
+                                                <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Open</TableCell>
+                                                <TableCell style={{backgroundColor: '#e9ecef'}} align="right">High</TableCell>
+                                                <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Low</TableCell>
+                                                <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Close</TableCell>
+                                                <TableCell align="right" style={{color: 'blue',backgroundColor: '#e9ecef'}}>Acciones</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        {(cryptosData.length != 0) ?
+                                                cryptosData.map((crypto) => (
+                                                        <TableBody>
+                                                            <TableRow style={{backgroundColor: '#BBE6E4'}}>
+                                                                <TableCell colSpan={7} align='center'>{crypto.name} - {crypto.shortName}</TableCell>
+                                                            </TableRow>
+                                                            <TableRow className='table-row'>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[0][0]}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[0][1].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[0][2].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[0][3].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[0][4].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[0][5].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right" rowSpan={2}>
+                                                                    <Button variant='outlined' onClick={() => copyRows(crypto.data[0], crypto.data[1])}>Copiar</Button>
+                                                                </TableCell>
+                                                            </TableRow>
+                                                            <TableRow className='table-row'>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[1][0]}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[1][1].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[1][2].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[1][3].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[1][4].toLocaleString("en-US")}</TableCell>
+                                                                <TableCell className='table-cell' align="right">{crypto.data[1][5].toLocaleString("en-US")}</TableCell>
+                                                                
+                                                            </TableRow>
+                                                        </TableBody>
+                                                ))
+                                        :
+                                        ''
+                                        }
+                                    </Table>
+                                </TableContainer>
+
+
+
+
+                            {/* Tab panel for each crypto currency and its table     */}
+                            </TabPanel>
+                            {cryptos.cryptos.map((crypto, i) =>
+                                <TabPanel>
+                                    <Grid textAlign={'left'}>
+                                        <h3>{crypto.name}</h3>
+                                        <TableContainer component={Paper}>
+                                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                                <TableHead>
+                                                    <TableRow>
+                                                        <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Date</TableCell>
+                                                        <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Value</TableCell>
+                                                        <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Open</TableCell>
+                                                        <TableCell style={{backgroundColor: '#e9ecef'}} align="right">High</TableCell>
+                                                        <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Low</TableCell>
+                                                        <TableCell style={{backgroundColor: '#e9ecef'}} align="right">Close</TableCell>
+                                                    </TableRow>
+                                                </TableHead>
+                                                {(cryptosData.length != 0) ?
+                                                    <TableBody>
+                                                    {cryptosData[i].data.map((value) => (
+                                                        <TableRow className='table-row'>
+                                                            <TableCell className='table-cell' align="right">{value[0]}</TableCell>
+                                                            <TableCell className='table-cell' align="right">{value[1]}</TableCell>
+                                                            <TableCell className='table-cell' align="right">{value[2]}</TableCell>
+                                                            <TableCell className='table-cell' align="right">{value[3]}</TableCell>
+                                                            <TableCell className='table-cell' align="right">{value[4]}</TableCell>
+                                                            <TableCell className='table-cell' align="right">{value[5]}</TableCell>
+                                                        </TableRow>
+                                                    ))}
+                                                </TableBody>
+                                                :
+                                                ''
+                                                }
+                                            </Table>
+                                        </TableContainer>
+                                    </Grid>
+                                </TabPanel>
+                            )}
+                        </SwipeableViews>
+                    </Box>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
